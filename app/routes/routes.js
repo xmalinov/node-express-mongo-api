a554de01763f85af011159e2a10ee7d80216b50d
Alexander Malinov <malinovbox@gmail.com>
2017-02-12 23:40:32 +0300

    Add Create route

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,1 +1,6 @@
-module.exports = function (app, db) {};
+module.exports = function (app, db) {
+    app.post('/notes', (req, res) => {
+        // CREATE route
+        res.send('Hello');
+    });
+};
