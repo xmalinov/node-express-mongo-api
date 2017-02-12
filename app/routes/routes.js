da6e13b9f42e8c039a4f5b8f98ff1c3aebbf855c
Alexander Malinov <malinovbox@gmail.com>
2017-02-12 23:51:20 +0300

    Add urlencoding for post bodies

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,7 @@
 module.exports = function (app, db) {
     app.post('/notes', (req, res) => {
         // CREATE route
+        console.log(req.body);
         res.send('Hello');
     });
 };
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = 8000;
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 require('./app/routes')(app, {});
 
 app.listen(port, () => {
