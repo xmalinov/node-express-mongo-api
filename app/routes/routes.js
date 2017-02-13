bb9f07b9da4c16509c99df5260df6e7ea1cd8af7
Alexander Malinov <malinovbox@gmail.com>
2017-02-13 22:57:12 +0300

    Add read, update and delete transactions

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,60 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function (app, db) {
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        const note = {
+            text: req.body.body,
+            title: req.body.title
+        };
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
     app.post('/notes', (req, res) => {
-        // CREATE route
         // console.log(req.body);
         // res.send('Hello');
         const note = {
